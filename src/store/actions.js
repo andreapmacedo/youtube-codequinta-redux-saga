054b1737dba209523474189b3fc39bcc23b258ec
andreapmacedo <aa.andremacedo@gmail.com>
2023-06-12 15:11:18 -0300

    feat: implementa redux saga basico.

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,17 @@
+// export function addTodo(text) {
+//   return {
+//     type: 'ADD_TODO',
+//     payload: { text },
+//   }
+// }
+
+// No lugar de disparar uma action diretamente,
+//  vamos disparar uma action que será interceptada pelo saga,
+// que fará a chamada à API e disparará uma nova action com
+//  os dados retornados pela API.
 export function addTodo(text) {
   return {
-    type: 'ADD_TODO',
+    type: 'ASYNC_ADD_TODO',
     payload: { text },
   }
 }
--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,31 +1,56 @@
-import { delay } from 'redux-saga';
-import { takeLatest, put, call, select } from 'redux-saga/effects';
 
-function apiGet(text, length) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve([
-        { id: 1, text: 'Fazer café' },
-        { id: 2, text: 'Fazer café 2' },
-        { id: 3, text: 'Fazer café 3' },
-        { id: 4, text: 'Fazer café 4' },
-      ]);
-    }, 2000);
-  });
-}
 
-function* getTodoList() {
-  try {
-    const response = yield call(apiGet); // call é usado para chamar funções que retornam promises
+import { takeLatest, put } from 'redux-saga/effects';
 
-    yield put({ type: 'SUCCESS_TODO_LIST', payload: { data: response } }); // put é usado para disparar uma action
-  } catch (err) {
-    yield put({ type: 'FAILURE_TODO_LIST' });
-  }
+// function* asyncAddTodo() {
+//   console.log('addTodo');
+//   yield put({ type: 'ADD_TODO', payload: { text: 'Fazer café' } });
+// }
+
+// Aproveitando e trazendo a action para o saga por meio do parâmetro action:
+function* asyncAddTodo(action) {
+  console.log('addTodo');
+  yield put({ type: 'ADD_TODO', payload: { text: action.payload.text } });
 }
 
 export default function* root() {
   yield [
-    takeLatest('REQUEST_TODO_LIST', getTodoList), // takeLatest é usado para pegar apenas a última chamada
+    takeLatest('ASYNC_ADD_TODO', asyncAddTodo), // takeLatest é usado para pegar apenas a última chamada
   ];
 }
+
+
+
+
+
+// import { delay } from 'redux-saga';
+// import { takeLatest, put, call, select } from 'redux-saga/effects';
+
+// function apiGet(text, length) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve([
+//         { id: 1, text: 'Fazer café' },
+//         { id: 2, text: 'Fazer café 2' },
+//         { id: 3, text: 'Fazer café 3' },
+//         { id: 4, text: 'Fazer café 4' },
+//       ]);
+//     }, 2000);
+//   });
+// }
+
+// function* getTodoList() {
+//   try {
+//     const response = yield call(apiGet); // call é usado para chamar funções que retornam promises
+
+//     yield put({ type: 'SUCCESS_TODO_LIST', payload: { data: response } }); // put é usado para disparar uma action
+//   } catch (err) {
+//     yield put({ type: 'FAILURE_TODO_LIST' });
+//   }
+// }
+
+// export default function* root() {
+//   yield [
+//     takeLatest('REQUEST_TODO_LIST', getTodoList), // takeLatest é usado para pegar apenas a última chamada
+//   ];
+// }
